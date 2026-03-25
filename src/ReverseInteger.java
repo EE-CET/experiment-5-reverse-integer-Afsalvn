4c6c0db0b040fcea4b148be9c5da0891f7bc874f
Afsalvn <147943587+Afsalvn@users.noreply.github.com>
2026-03-25 12:12:38 +0530

    Implement integer reversal in ReverseInteger class

--- src/ReverseInteger.java
+++ src/ReverseInteger.java
@@ -1,3 +1,18 @@
-public class ReverseInteger { 
-        // TODO: Read an integer and print its reverse  
+import java.util.Scanner;
+
+public class ReverseInteger {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int reversed = 0;
+        
+        while (n != 0) {
+            int digit = n % 10;
+            reversed = reversed * 10 + digit;
+            n /= 10;
+        }
+        
+        System.out.println(reversed);
+        scanner.close();
+    }
 }
